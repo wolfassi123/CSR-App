8bc8ab63cfd9fc166367fa7d72409092cb1d5a0f
Wolf Assi <wolf.assi@net.usj.edu.lb>
2021-05-11 13:32:29 +0300

    Removal of some comments.

--- models/users.js
+++ models/users.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var passportLocalMongoose =require('passport-local-mongoose');
-//require('mongoose-currency').loadType(mongoose);
-//const Currency = mongoose.Types.Currency;
+
 
 const userSchema = new Schema({
     admin: {
